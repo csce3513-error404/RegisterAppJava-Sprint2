d50dbcae7af6652c4fbbfd5cbe6d9699e30ecc80
scottj98 <44309841+scottj98@users.noreply.github.com>
2020-04-02 14:50:08 -0500

    Add files via upload

---
+++ src/main/java/edu/uark/registerapp/commands/products/ProductQuantity.java
@@ -0,0 +1,25 @@
+package edu.uark.registerapp.commands.products;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ProductEntity;
+import edu.uark.registerapp.models.repositories.ProductRepository;
+
+@Service
+public class ProductQuantity implements ResultCommandInterface<Product> {
+	@Transactional
+	@Override
+	public Product run() {
+		// some stuff
+	}
+}
