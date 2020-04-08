b6f939dedb7641a72e7bca9ab4790f6a10a79a50
sebo2213 <sebo2213.tesla@gmail.com>
2020-04-08 13:46:50 -0500

    test cases & inCart for product model

---
+++ src/test/java/edu/uark/registerapp/ProductObjectTests.java
@@ -0,0 +1,17 @@
+package edu.uark.registerapp;
+
+import org.junit.*;
+
+public class ProductObjectTests {
+//----------------------------------------- These tests are for the data fields inside the object to ensure that they work properly when called -------------------------------------------------//
+    @Test
+    public void inCartTest(){
+
+    }
+
+    @Test
+    public void idTest(){ }
+
+    @Test
+    public void lookUpCodeTest(){ }
+}
