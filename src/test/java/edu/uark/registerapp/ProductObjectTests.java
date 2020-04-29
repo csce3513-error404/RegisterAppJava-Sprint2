e71cd78e6159ace32fcb495c418ded9fb8e8e339
sebo2213 <sebo2213.tesla@gmail.com>
2020-04-28 22:50:28 -0500

    Revert "Revert "Fixing some errors that were accidently pushed onto the master branch""
    This reverts commit 43d4014d96f53fe30a69ed672420b79906b88d50.

--- src/test/java/edu/uark/registerapp/ProductObjectTests.java
+++ src/test/java/edu/uark/registerapp/ProductObjectTests.java
@@ -3,12 +3,22 @@ package edu.uark.registerapp;
 import org.junit.*;
 import edu.uark.registerapp.models.api.Product;
 
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class ProductObjectTests {
     //----------------------------------------- These tests are for the data fields inside the object to ensure that they work properly when called -------------------------------------------------//
+    public static Product product;
+
+    @Before
+    public void createThing(){
+        product = new Product();
+    }
+
     @Test
     public void inCartTest(){
-        Product product = new Product();
         product.setInCart(true);
         assertEquals(true, product.getInCart());
 
@@ -17,8 +27,16 @@ public class ProductObjectTests {
     }
 
     @Test
-    public void idTest(){ }
+    public void idTest(){
+        UUID id = UUID.randomUUID();
+        product.setId(id);
+        assertEquals(id, product.getId());
+    }
 
     @Test
-    public void lookUpCodeTest(){ }
+    public void lookUpCodeTest(){
+        String code = "I am a lookup code";
+        product.setLookupCode(code);
+        assertEquals("I am a lookup code", product.getLookupCode());
+    }
 }
