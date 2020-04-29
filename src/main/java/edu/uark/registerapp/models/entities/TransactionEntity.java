b88c5ae86d6323f430206c703eca22f1c42c2da1
sebo2213 <sebo2213.tesla@gmail.com>
2020-04-29 15:05:33 -0500

    Update TransactionEntity.java

--- src/main/java/edu/uark/registerapp/models/entities/TransactionEntity.java
+++ src/main/java/edu/uark/registerapp/models/entities/TransactionEntity.java
@@ -8,24 +8,46 @@ import java.util.UUID;
 @Table(name="transaction")
 public class TransactionEntity {
 
-    @Column(name = "transaction")
-    private ArrayList transaction;
+//    @Column(name = "transaction")
+//    private ArrayList transaction;
+//
+//    public ArrayList getTransaction(){
+//        return this.transaction;
+//    }
 
-    public ArrayList getTransaction(){
-        return this.transaction;
-    }
-
-    public TransactionEntity setTransaction(ArrayList transaction){
-        this.transaction = transaction;
-        return this;
-    }
+//    public TransactionEntity setTransaction(ArrayList transaction){
+//        this.transaction = transaction;
+//        return this;
+//    }
 
     @Id
     @Column(name="T_id", updatable = false)
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private static UUID transactionID;
+    private final UUID transactionID;
 
-    public UUID getId() {
+    public UUID get_T_Id() {
         return this.transactionID;
+    }
+
+    @Column(name="employee_ID")
+    private UUID employee_ID;
+
+    public UUID getEmployee_ID() {
+        return employee_ID;
+    }
+
+    public void setEmployee_ID(UUID employee_ID) {
+        this.employee_ID = employee_ID;
+    }
+
+    @Column(name="product_IDs")
+    private String product_IDs; //list of product IDs will be , delimitated
+
+    public String getProduct_IDs() {
+        return product_IDs;
+    }
+
+    public void setProduct_IDs(String product_IDs) {
+        this.product_IDs = product_IDs;
     }
 }
