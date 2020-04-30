0dbf4068758958044bcfacd566a1a62b482b0e46
sebo2213 <sebo2213.tesla@gmail.com>
2020-04-30 12:35:03 -0500

    Revert "Update RegisterApplication.java"
    This reverts commit 7c9ac2013ff2e185b28a8a714cf3b513a0d6f3b1.

--- src/main/java/edu/uark/registerapp/RegisterApplication.java
+++ src/main/java/edu/uark/registerapp/RegisterApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"edu.uark"})
 public class RegisterApplication {
 
 	public static void main(final String[] args) {
