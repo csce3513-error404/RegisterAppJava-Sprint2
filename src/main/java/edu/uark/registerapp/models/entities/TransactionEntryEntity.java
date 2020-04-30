1ef57c1cb37dfcf9924146da0f5f6e3d8a4257a4
scottj98 <44309841+scottj98@users.noreply.github.com>
2020-04-30 13:43:33 -0500

    Create TransactionEntryEntity.java

---
+++ src/main/java/edu/uark/registerapp/models/entities/TransactionEntryEntity.java
@@ -0,0 +1,85 @@
+package edu.uark.registerapp.models.entities;
+
+import edu.uark.registerapp.models.api.TransactionEntry;
+
+import javax.persistence.*;
+import javax.util.UUID;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="transactionEntry")
+public class TransactionEntryEntity {
+	@Id
+	@Column(name="id", updatable=false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private final UUID id;
+	
+	public UUID getId() {
+		return this.id;
+	}
+	
+	
+	@Column(name="transaction_Id)
+	private UUID transactionId;
+	
+	public UUID getTransactionId() {
+		return this.transactionId;
+	}
+	public TransactionEntryEntity setTransactionId(final UUID transactionId) {
+		this.transactionId =transactionId;
+		return this;
+	}
+	
+	
+	@Column(name="product_Id)
+	private UUID productId;
+	
+	public UUID getProductId() {
+		return this.productId;
+	}
+	public TransactionEntryEntity setProductId(final UUID productId) {
+		this.productId = productId;
+		return this;
+	}
+	
+	
+	@Column(name="quantity")
+	private int quantity;
+	
+	public int getQuantity() {
+		return this.quantity;
+	}
+	public TransactionEntryEntity setQuantity(final int quantity) {
+		this.quantity = quantity;
+		return this;
+	}
+	
+	
+	@Column(name="price")
+	private int price;
+	
+	public int getPrice() {
+		return this.price;
+	}
+	public TransactionEntryEntity setPrice(final int price) {
+		this.quantity = quantity;
+		return this;
+	}
+	
+	
+	public TransactionEntryEntity() {
+		this.id = new UUID(0, 0);
+		this.transactionId = new UUID(0, 0);
+		this.productId = new UUID(0, 0);
+		this.quantity = 0;
+		this.price = 0;
+	}
+	
+	public TransactionEntryEntity(final UUID transactionId, final UUID productId, final int quantity, final int price) {
+		this.id = new UUID(0, 0);
+		this.transactionId = transactionId;
+		this.productId = productId;
+		this.quantity = quantity;
+		this.price = price;
+	}
+}
